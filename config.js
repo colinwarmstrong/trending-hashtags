491044d3bed842438373faec7edb5e15bc8368a6
Colin Armstrong <colinwarmstrong@gmail.com>
2024-11-23 19:48:25 -0700

    Linting

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 const env = process.env
 
 const redisConnection = {
-    url: env.REDIS_URL || "redis://localhost:6379",
-    password: env.REDIS_PASSWORD || "super-secret-password-123"
+  url: env.REDIS_URL || 'redis://localhost:6379',
+  password: env.REDIS_PASSWORD || 'super-secret-password-123'
 }
 
 export const config = {
-    express: {
-        port: env.EXPRESS_PORT || 8080
-    },
+  express: {
+    port: env.EXPRESS_PORT || 8080
+  },
 
-    redis: redisConnection,
+  redis: redisConnection,
 
-    jobQueueConfig: {
-        connection: redisConnection
-    },
+  jobQueueConfig: {
+    connection: redisConnection
+  },
 
-    workerConfig: {
-        connection: redisConnection,
-        concurrency: parseInt(env.WORKER_CONCURRENCY) || 50
-    }
+  workerConfig: {
+    connection: redisConnection,
+    concurrency: parseInt(env.WORKER_CONCURRENCY) || 50
+  }
 }
